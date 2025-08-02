6e7a0c497877b2948dae745d7264208c8ef7db74
saamiatajani <aamirsaamia@gmail.com>
2025-08-02 15:32:12 -0700

    chap 2 comp

--- chap 2/apps.js
+++ chap 2/apps.js
@@ -10,4 +10,38 @@ alert(myName)
 // b . Assign “Hello World” to variable message 
 var message="Hello world"
 // c) Display the message in alert box.               
-alert(message)
+
+
+// Q4. Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+ var stuName="Shayan"
+alert(stuName)
+var hisAge="13 years"
+alert(hisAge)
+ var courseDetail="certified as WMA"
+alert(courseDetail)
+
+// Q5 .Write a script to display the following alert using one JS
+// variable:
+var pizza="Pizza\nPizz\nPiz\nPi\nP"
+alert(pizza)
+
+// Q6. Declare a variable called email and assign to it a string that
+//  represents your Email Address(e.g. example@example.com).
+//  Show the blow mentioned message in an alert box.(Hint: use
+//  string concatenation)
+var stMail="aamirsaamia@gmail.com"
+alert("My email is" + stMail)
+
+// Q7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+var book="A smart\n way to learning javascript"
+alert("Iam trying to learn" + book)
+
+// Q8.
+document.write("Yay! I can type HTML content through Js")
+
+// Q9 .
+var sign="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(sign)
