2ab6cc81c143898c75839727a1148f50751977af
saamiatajani <aamirsaamia@gmail.com>
2025-08-02 14:24:51 -0700

    js practice

---
+++ chap 2/apps.js
@@ -0,0 +1,13 @@
+// Q1 Declare a variable called username. 
+var username;
+
+// Q2 Declare a variable called myName & assign to it a string  that represents your Full Name.
+var myName ="My name is Saamia"
+alert(myName)
+
+// Q3 Write script to 
+// a. Declare a JS variable, titled message.
+// b . Assign “Hello World” to variable message 
+var message="Hello world"
+// c) Display the message in alert box.               
+alert(message)
