942c30af49617206b26a3d731ced8d5b8bf1bfb7
saamiatajani <aamirsaamia@gmail.com>
2025-08-03 07:25:34 -0700

    Message

---
+++ chap 3/apps.js
@@ -0,0 +1,29 @@
+// Q1. Declare a variable called age & assign to it your age. Show
+// your age in an alert box.
+var age ="I am 19 years old"
+alert(age)
+
+// Q2. Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+var visitorhasVisited="You have visited 5 times"
+alert(visitorhasVisited)
+
+// Q3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+var birthYear="My birth year is 2006"
+document.write(birthYear);
+
+// Q4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+var visitorName="Shayan Tajani"
+var productTitle="T-shirt"
+var Quantity="7"
+document.write(visitorName + " " + "ordered"+ " "+ Quantity+ " " +productTitle);
