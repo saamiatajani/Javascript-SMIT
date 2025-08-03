9945d05d7f7c70d0bbbf29780bc2aa2bb39c5894
saamiatajani <aamirsaamia@gmail.com>
2025-08-03 06:57:10 -0700

    chap 1 edit

---
+++ chap 1/app.js
@@ -0,0 +1,23 @@
+// Q1 .Write a script to greet your website visitor using JS alert
+// box.
+alert("Welcome to Myweb")
+
+// Q2. Write a script to display following message on your web
+// page:
+alert("Trouble! Enter a correct password")
+
+// Q3. Write a script to display following message on your web
+// page:
+alert("Have a safe\njourney")
+
+// Q4. Write a script to display following messages in sequence:
+alert("Welcome to Js land...")
+alert("Happy coding! ")
+
+// Q5. Generate the following message through browser’s
+// developer console:
+alert("Hey! I can run Js through web developer console")
+
+// Q6. Make use of alerts in your new/existing HTML & CSS
+// project.
+
