622006978b1994a3e78e266ac8d6c59b4a5aae67
saamiatajani <aamirsaamia@gmail.com>
2025-08-04 10:09:03 -0700

    Message

---
+++ chap 4/apss.js
@@ -0,0 +1,35 @@
+// Q1. Declare 3 variables in one statement.
+var name = "John", age = 25, city = "New York";
+document.write("Name: " + name + "<br>");
+document.write("Age: " + age + "<br>");
+document.write("City: " + city);
+
+// Q2. Declare 5 legal & 5 illegal variable names.
+// 5 Legal variable
+var userName;
+var age1;
+var $dollar;
+var _privateParty;
+var cityName;
+//  5 illegal
+// var 1name;
+// var user-name;
+// var @city;
+// var var ;
+// var first name;
+
+// QDisplay this in your browser
+// a) A heading stating “Rules for naming JS variables”
+document.write("<h1>Rules for naming JS variables</h1>");
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+document.write("Variable names can only contain letters, numbers, $ and _. <br>");
+document.write("For example $Saamia,_Aamir or Tajani <br><b><br><br>")
+// c) Variables must begin with a ______, ______ or
+// _____.
+document.write("Variables must begin with a letter, $ or _. <br>");
+document.write("For example: $name, _name or name<br><br><br><br>");
+// d) Variable names are case _________
+document.write("Variable names are case sensitive.<br><br>");
+// e) Variable names should not be JS _________
+document.write("Variable names should not be JS keywords.");
